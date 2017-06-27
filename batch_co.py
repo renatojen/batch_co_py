28e73f73157703a5c988818fe1f7a0f385b825de
Victor Salgado <victor.mcsalgado@gmail.com>
2017-06-27 16:10:11 -0300

    use enumerate function

--- batch_co.py
+++ batch_co.py
@@ -2,12 +2,10 @@ import glob
 from os import rename, path, makedirs
 from shutil import copy2
 
-i = 0
 prefix = "OP"
 suffix = ""
 if not path.exists('./new/'):
     makedirs('./new/')
-for filename in glob.iglob('./level1/**/*.jpg', recursive=True):
-    i = i + 1
+for i, filename in enumerate(glob.iglob('./level1/**/*.jpg', recursive=True)):
     print(str('{:06}'.format(i)) + ' - ' + filename + ' - ' + path.basename(filename) + ' - ' + path.dirname(path.realpath(filename)))
     copy2(filename, './new/' + prefix + str('{:06}'.format(i)) + suffix +  ".jpg")
